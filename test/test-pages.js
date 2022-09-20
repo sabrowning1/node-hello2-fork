7a39d4850c799a8213139816b669236d68a7118d
Sam Browning <106113886+sabrowning1@users.noreply.github.com>
2022-09-20 09:37:24 -0400

    Update test-pages.js

--- test/test-pages.js
+++ test/test-pages.js
@@ -6,7 +6,7 @@ it('Page content test', (done) => {
   request.get('http://localhost:3000', function (err, res, body) {
       expect(res.statusCode).to.equal(200);
       expect(res).to.have.property('body');
-      expect(res).property('body').to.match(/Hello world, everything is awesome/);
+      expect(res).property('body').to.match(/Hi friends, everything is awesome/);
       done();
   });
 });
